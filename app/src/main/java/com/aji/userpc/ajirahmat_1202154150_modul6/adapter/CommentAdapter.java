e9c1bea0e746cc3aa8211d104ed8e99b76c1a2cd
ajirahmat <ajirahmatmuhajir11@gmail.com>
2018-04-02 01:08:31 +0700

    alhamdulillah

---
+++ app/src/main/java/com/aji/userpc/ajirahmat_1202154150_modul6/adapter/CommentAdapter.java
@@ -0,0 +1,80 @@
+package com.aji.userpc.ajirahmat_1202154150_modul6.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.aji.userpc.ajirahmat_1202154150_modul6.R;
+import com.aji.userpc.ajirahmat_1202154150_modul6.model.Comment;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
+    //inisiasi variable
+    Context context;
+    List<Comment> commentList;
+
+
+    //constructor
+    public CommentAdapter(Context context, List<Comment> commentList) {
+        this.context = context;
+        this.commentList = commentList;
+    }
+
+    //melakukan inisiasi variable
+    //inlfate layout
+    //melakukan bind item pada layout
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mUsername;
+        TextView mComment;
+        ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.img_avatar);
+            mUsername=itemView.findViewById(R.id.tv_username);
+            mComment=itemView.findViewById(R.id.tv_comment);
+        }
+    }
+
+    //inflate layout
+
+    @NonNull
+    @Override
+    public CommentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_comment,parent,false);
+        return new ViewHolder(v);
+    }
+
+    //bind item pada layout
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Comment comment= commentList.get(position);
+
+        holder.mUsername.setText(comment.getUsername());
+        holder.mComment.setText(comment.getComment());
+//        setupImageRounded();
+        holder.imageView.setImageResource(R.drawable.ic_account_circle_black_24dp);
+
+    }
+//    private void setupImageRounded() {
+//        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.test);
+//        rounded = RoundedBitmapDrawableFactory.create(context.getResources(), bitmap);
+//        rounded.setCircular(true);
+//
+//    }
+
+    @Override
+    public int getItemCount() {
+        return commentList.size();
+    }
+
+
+
+}
